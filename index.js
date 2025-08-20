232eb9ada02c136d6c16a92076b2aa80349e52c3
Rakibul Islam <rakib.ca@gmail.com>
2025-08-19 23:44:01 -0600

    parsing data from csv file

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const parse = require('csv-parse');
+const { parse } = require('csv-parse');
 const fs = require('fs');
 
 const results = [];
 
 fs.createReadStream('kepler_data.csv')
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
   .on('data', (data) => {
     results.push(data);
   })
@@ -14,5 +20,3 @@ fs.createReadStream('kepler_data.csv')
     console.log(results);
     console.log('done');
   });
-
-// parse();
